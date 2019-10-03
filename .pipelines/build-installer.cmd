e2871a79fb20ad6429a3b6af17574cd54974cdc3
Sasha Pierson <sashapie@microsoft.com>
2019-10-03 14:46:08 -0700

    robocopy to get around wix issue

--- .pipelines/build-installer.cmd
+++ .pipelines/build-installer.cmd
@@ -1,15 +1,45 @@
-cd /D "%~dp0"
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat -arch=amd64 -host_arch=amd64 -winsdk=10.0.16299.0"
-call msbuild ../installer/PowerToysSetup.sln /p:Configuration=Release /p:Platform=x64 || exit /b 1
+:SETUP
+set EXPECTED_OUTPUT="PowerToysSetup/x64/Release"
+pushd
 
-dir
-cd ..
-dir
-cd installer
-dir
-cd PowerToysSetup
-dir
-cd x64
-dir
-cd Release
-dir
+:COPYSOURCE
+call robocopy ../installer/ %tmp%/infrabuild/ /s
+if errorlevel 7 goto FAIL
+
+pushd %tmp%/infrabuild
+
+:BUILD
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat -arch=amd64 -host_arch=amd64 -winsdk=10.0.16299.0"
+call msbuild PowerToysSetup.sln /p:Configuration=Release /p:Platform=x64 || exit /b 1
+
+:CHECKRESULTS
+echo Expected Output: %EXPECTED_OUTPUT%
+
+if not exist %EXPECTED_OUTPUT% (
+  echo NO build output folder
+  goto FAIL
+)
+
+:COPYOUTPUT
+popd
+call robocopy %tmp%/infrabuild/PowerToysSetup/x64/ ./installer/PowerToysSetup/x64 /s
+
+:NORMALFINISH
+::errorlevel greater than 7 - is intentional. see
+:: https://blogs.technet.microsoft.com/deploymentguys/2008/06/16/robocopy-exit-codes/
+::for more details
+
+if errorlevel 7 goto FAIL
+
+echo Build SUCCEEDED.
+set exit_code=0
+
+goto END
+
+:FAIL
+echo Build FAILED.
+set exit_code=1
+
+:END
+echo popd
+popd
