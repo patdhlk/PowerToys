293815524aff9e4cc5a9aeae613a25da107080c9
Sasha Pierson <sashapie@microsoft.com>
2019-10-03 17:07:36 -0700

    build then task list - wix open?

--- .pipelines/build-installer.cmd
+++ .pipelines/build-installer.cmd
@@ -1,50 +1,53 @@
 :SETUP
 cd /D "%~dp0"
-set EXPECTED_OUTPUT="PowerToysSetup\x64\Release"
+set EXPECTED_OUTPUT="installer\PowerToysSetup\x64\Release"
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat -arch=amd64 -host_arch=amd64 -winsdk=10.0.16299.0"
-pushd
+REM pushd
 
-:COPYSOURCE
-dir
-call robocopy ..\installer\ %tmp%\infrabuild\ /s
-if errorlevel 7 goto FAIL
+REM :COPYSOURCE
+REM dir
+REM call robocopy ..\installer\ %tmp%\infrabuild\ /s
+REM call robocopy ..\x64 %tmp%\infrabuild /s
+REM if errorlevel 7 goto FAIL
 
-dir %tmp%
-pushd %tmp%\infrabuild
+REM dir %tmp%
+REM pushd %tmp%\infrabuild
 
 :BUILD
 
 call msbuild PowerToysSetup.sln /p:Configuration=Release /p:Platform=x64 || exit /b 1
 
-:CHECKRESULTS
-echo Expected Output: %EXPECTED_OUTPUT%
+tasklist /V 
 
-if not exist %EXPECTED_OUTPUT% (
-  dir
-  echo NO build output folder
-  goto FAIL
-)
+REM :CHECKRESULTS
+REM echo Expected Output: %EXPECTED_OUTPUT%
 
-:COPYOUTPUT
-popd
-call robocopy %tmp%\infrabuild\PowerToysSetup\x64\ .\installer\PowerToysSetup\x64 /s
+REM if not exist %EXPECTED_OUTPUT% (
+REM   dir
+REM   echo NO build output folder
+REM   goto FAIL
+REM )
 
-:NORMALFINISH
-::errorlevel greater than 7 - is intentional. see
-:: https://blogs.technet.microsoft.com/deploymentguys/2008/06/16/robocopy-exit-codes/
-::for more details
+REM :COPYOUTPUT
+REM popd
+REM call robocopy %tmp%\infrabuild\PowerToysSetup\x64\ .\installer\PowerToysSetup\x64 /s
 
-if errorlevel 7 goto FAIL
+REM :NORMALFINISH
+REM ::errorlevel greater than 7 - is intentional. see
+REM :: https://blogs.technet.microsoft.com/deploymentguys/2008/06/16/robocopy-exit-codes/
+REM ::for more details
 
-echo Build SUCCEEDED.
-set exit_code=0
+REM if errorlevel 7 goto FAIL
 
-goto END
+REM echo Build SUCCEEDED.
+REM set exit_code=0
 
-:FAIL
-echo Build FAILED.
-set exit_code=1
+REM goto END
 
-:END
-echo popd
-popd
+REM :FAIL
+REM echo Build FAILED.
+REM set exit_code=1
+
+REM :END
+REM echo popd
+REM popd
