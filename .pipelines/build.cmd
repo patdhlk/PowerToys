2d2468e50bd20bbfb46771a4549a32e438ad82dc
Sasha Pierson <RedSquirrelious@users.noreply.github.com>
2019-10-07 09:24:48 -0700

    poke around in new container

--- .pipelines/build.cmd
+++ .pipelines/build.cmd
@@ -1,4 +1,17 @@
 cd /D "%~dp0"
 REM set MSBUILD_EXE="C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe"
+
+dir "C:\Program Files (x86)"
+
+dir "C:\Program Files (x86)\Microsoft Visual Studio\"
+
+dir "C:\Program Files (x86)\Microsoft Visual Studio\2019\"
+
+dir "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\"
+
+dir "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\"
+
+dir "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\"
+
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat -arch=amd64 -host_arch=amd64 -winsdk=10.0.16299.0"
-call msbuild ../PowerToys.sln /p:Configuration=Release /p:Platform=x64 || exit /b 1
+call %MSBUILD_EXE% ../PowerToys.sln /p:Configuration=Release /p:Platform=x64 || exit /b 1
