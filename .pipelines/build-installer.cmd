987a50577e08e4554959baaa9f14e7c7868cde94
Sasha Pierson <sashapie@microsoft.com>
2019-10-07 21:26:56 -0700

    build installer wo robocopy

--- .pipelines/build-installer.cmd
+++ .pipelines/build-installer.cmd
@@ -1,41 +1,45 @@
 :SETUP
 cd /D "%~dp0"
-set EXPECTED_OUTPUT="installer\PowerToysSetup\x64\Release"
+set EXPECTED_OUTPUT="..\installer\PowerToysSetup\x64\Release"
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat -arch=amd64 -host_arch=amd64 -winsdk=10.0.16299.0"
 REM set MSBUILD_EXE="C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\15.0\Bin\MSBuild.exe"
 REM pushd
 
 REM :COPYSOURCE
 REM dir
-call robocopy ..\installer %tmp%\infrabuild\installer /s
-call robocopy ..\x64 %tmp%\infrabuild\x64 /s
-call robocopy .. %tmp%\infrabuild License.rtf
-call robocopy ../src %tmp%\infrabuild\src /s
+REM call robocopy ..\installer %tmp%\infrabuild\installer /s
+REM call robocopy ..\x64 %tmp%\infrabuild\x64 /s
+REM call robocopy .. %tmp%\infrabuild License.rtf
+REM call robocopy ../src %tmp%\infrabuild\src /s
 REM if errorlevel 7 goto FAIL
 
-dir %tmp%
-dir %tmp%\infrabuild
-dir %tmp%\infrabuild\x64
+REM dir %tmp%
+REM dir %tmp%\infrabuild
+REM dir %tmp%\infrabuild\x64
 REM pushd %tmp%\infrabuild
 
 :BUILD
 
-REM call msbuild ../installer/PowerToysSetup.sln /p:Configuration=Release /p:Platform=x64 || exit /b 1
+call msbuild ../installer/PowerToysSetup.sln /p:Configuration=Release /p:Platform=x64 || exit /b 1
 
-call msbuild %tmp%\infrabuild\installer\PowerToysSetup.sln /p:Configuration=Release /p:Platform=x64 || exit /b 1
+dir ../installer/PowerToysSetup/
+dir ../installer/PowerToysSetup/x64/
+dir ../installer/PowerToysSetup/x64/Release
 
-dir %tmp%\infrabuild\installer\PowerToysSetup
-dir %tmp%\infrabuild\installer\PowerToysSetup\x64
-dir %tmp%\infrabuild\installer\PowerToysSetup\x64\Release
+REM call msbuild %tmp%\infrabuild\installer\PowerToysSetup.sln /p:Configuration=Release /p:Platform=x64 || exit /b 1
 
-:CHECKRESULTS
-echo Expected Output: %EXPECTED_OUTPUT%
+REM dir %tmp%\infrabuild\installer\PowerToysSetup
+REM dir %tmp%\infrabuild\installer\PowerToysSetup\x64
+REM dir %tmp%\infrabuild\installer\PowerToysSetup\x64\Release
 
-if not exist %EXPECTED_OUTPUT% (
-  dir
-  echo NO build output folder
-  goto FAIL
-)
+REM :CHECKRESULTS
+REM echo Expected Output: %EXPECTED_OUTPUT%
+
+REM if not exist %EXPECTED_OUTPUT% (
+REM   dir
+REM   echo NO build output folder
+REM   goto FAIL
+REM )
 
 REM :COPYOUTPUT
 REM popd
